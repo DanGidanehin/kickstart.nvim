81eb8a5c496785016d919edb09f4581dd5c78b0b
Zub Denys <zub.deniss@gmail.com>
2025-11-14 18:34:45 +0100

    remove bins paths from init.lua
    I just don't need it, it was temporary solution

--- lua/denys/core/init.lua
+++ lua/denys/core/init.lua
@@ -1,12 +1,3 @@
--- Make sure Neovim/Mason can find cargo and Homebrew bins
-local home = vim.env.HOME
-vim.env.PATH = table.concat({
-  home .. "/.cargo/bin",
-  "/opt/homebrew/bin",
-  "/usr/local/bin",
-  vim.env.PATH or "",
-}, ":")
-
 require("denys.core.options")
 require("denys.core.keymaps")
 require("denys.core.open-folder")
