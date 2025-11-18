a4f368be9978e67c235e832928503ba7d69e2954
Zub Denys <zub.deniss@gmail.com>
2025-11-18 12:05:36 +0100

    update telescope with plugin search and multigrep
    - create telescope/multigrep.lua for multigrep functionality
    - added multigrep and packages search for telescope inside telescope.lua

--- lua/denys/plugins/telescope.lua
+++ lua/denys/plugins/telescope.lua
@@ -27,16 +27,18 @@ return {
 
     telescope.load_extension("fzf")
 
-    -- set keymaps
+    -- Set keymaps
     local keymap = vim.keymap
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Find todos" })
+    keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Find keymaps" })
     keymap.set("n", "<leader>fp", function()
       require("telescope.builtin").find_files({
         cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
       })
     end)
+
+    require("denys.plugins.telescope.multigrep").setup()
   end,
 }
